5fbae8215e00372d051832f98b795a1af312349e
Juverio <juverio@gmail.com>
2025-01-13 13:38:56 +0700

    Updated

--- models/model.js
+++ models/model.js
@@ -1,8 +1,21 @@
 import { Sequelize, DataTypes } from "sequelize";
+<<<<<<< HEAD
 const sequelize = new Sequelize("netflix", "root", "root", {
   host: "mysql",
   dialect: "mysql",
 });
+=======
+
+const sequelize = new Sequelize(
+  "netflix", 
+  process.env.USER_NAME,
+  process.env.USER_PWD,
+  {
+    host: process.env.DB_URL,
+    dialect: "mysql",
+  }
+);
+>>>>>>> aec44aa (Updated)
 
 (async () => {
   try {
